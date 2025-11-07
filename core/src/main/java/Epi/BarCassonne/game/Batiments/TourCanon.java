c19a2e363aad9655faeac5b85f0e5ec260b0ab80
Baixerastheo <theo.baixeras@gmail.com>
2025-11-07 15:07:40 +0100

    Batiment package up

--- core/src/main/java/Epi/BarCassonne/game/Batiments/TourCanon.java
+++ core/src/main/java/Epi/BarCassonne/game/Batiments/TourCanon.java
@@ -1,4 +1,7 @@
 package Epi.BarCassonne.game.Batiments;
 
-public class TourCanon {
+public class TourCanon extends Batiment {
+    public TourCanon (float positionX, float positionY, int level, int maxLevel, float damage, float portee) {
+        super(positionX, positionY, level, maxLevel, damage, portee);
+    }
 }
